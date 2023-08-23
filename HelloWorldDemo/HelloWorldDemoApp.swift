0ce24094a59c77645461f94fcd18617d1815269a
CalTaylor <s0357576@student.rockvalleycollege.edu>
2023-08-23 16:54:58 -0500

    Initial Commit

---
+++ HelloWorldDemo/HelloWorldDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldDemoApp.swift
+//  HelloWorldDemo
+//
+//  Created by Cal M. Taylor on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
